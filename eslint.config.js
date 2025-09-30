75761416f84051e058fc2921c44dd8e9213d72cf
Valeriia Salata <lerasalata@gmail.com>
2025-09-30 10:58:26 +0300

    fix: added eslint config file

---
+++ eslint.config.js
@@ -0,0 +1,24 @@
+import js from "@eslint/js";
+import tseslint from "typescript-eslint";
+import vue from "eslint-plugin-vue";
+
+export default [
+  {
+    ignores: ["**/.output/**", "**/.nuxt/**", "**/node_modules/**"],
+  },
+  js.configs.recommended,
+  ...tseslint.configs.recommended,
+  ...vue.configs["flat/recommended"],
+  {
+    files: ["**/*.ts", "**/*.vue"],
+    languageOptions: {
+      parserOptions: {
+        ecmaVersion: "latest",
+        sourceType: "module",
+      },
+    },
+    rules: {
+      "vue/multi-word-component-names": "off", 
+    },
+  },
+];
