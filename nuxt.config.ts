ae3786d15d8fa52d7cb886e96a440f670743957e
KyryloVorobiov <kvorobiov17@gmail.com>
2025-10-28 20:33:22 +0100

    зроблені сторінки для пошуку та дет. інформації фільма, підключено до бєка

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,4 +2,15 @@
 export default defineNuxtConfig({
   compatibilityDate: "2025-07-15",
   devtools: { enabled: true },
+
+  server: {
+    port: 3001, // фронт на 3001, бєк на 3000
+    host: 'localhost',
+  },
+
+  routeRules: {
+    '/api/**': {
+      proxy: 'http://localhost:3000/**'
+    }
+  },
 });
